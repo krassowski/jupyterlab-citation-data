a8b5a2ceccd14f7c2591959684f3b9e7739d9f79
krassowski <krassowski.michal@gmail.com>
2021-08-19 15:20:01 +0100

    Correct name, handle version better

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 """
 jupyterlab-citation-data setup
 """
+import ast
 from pathlib import Path
 
 import setuptools
@@ -8,11 +9,29 @@ from glob import glob
 
 HERE = Path(__file__).parent.resolve()
 
+
+def get_version(path: Path):
+    module = ast.parse(path.read_text('utf-8'))
+    for statement in module.body:
+        if not isinstance(statement, ast.Assign):
+            continue
+        target = statement.targets[0]
+        if not isinstance(target, ast.Name):
+            continue
+        if target.id != '__version__':
+            continue
+        constant = statement.value
+        if not isinstance(constant, ast.Constant):
+            continue
+        return constant.value
+
+
 long_description = (HERE / "README.md").read_text()
+version = get_version(HERE / 'jupyterlab_citation_manager' / '_version.py')
 
 setup_args = dict(
     name="jupyterlab-citation-data",
-    version='0.1',
+    version=version,
     url='https://github.com/krassowski/jupyterlab-citation-manager',
     author='Michał Krassowski',
     description='CSL styles for jupyterlab-citation-manager',
@@ -26,7 +45,10 @@ setup_args = dict(
     platforms="Linux, Mac OS X, Windows",
     keywords=["Jupyter", "JupyterLab", "JupyterLab3"],
     data_files=[
-        ("share/jupyter/csl-styles", glob(str(HERE / "csl-styles" / "**" / "*"), recursive=True))
+        ("share/jupyter/csl-styles", [
+            str(Path(path).relative_to(HERE))
+            for path in glob(str(HERE / "csl-styles" / "**" / "*"), recursive=True)
+        ])
     ],
     classifiers=[
         "Programming Language :: Python",
